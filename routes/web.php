d63cc00dfd1ff9650dfb78dd90bf0473e2b42d8b
adithyal-21 <adithya2120@gmail.com>
2021-05-19 20:29:37 +0530

    laravel

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+// Admin routes
+Route::prefix('admin')->group(function(){
+    Route::get('/', 'Users\Admin\AdminController@index')->name('admin.dashboard');
+    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+    Route::get('/register', 'Auth\AdminRegisterController@showRegisterForm')->name('admin.register');
+    Route::post('/register', 'Auth\AdminRegisterController@register')->name('admin.register.submit');
+});
+
+
+// Vendor routes
+Route::prefix('vendor')->group(function(){
+    Route::get('/', 'Users\Vendor\VendorController@index')->name('vendor.dashboard');
+    Route::get('/login', 'Auth\VendorLoginController@showLoginForm')->name('vendor.login');
+    Route::post('/login', 'Auth\VendorLoginController@login')->name('vendor.login.submit');
+    Route::get('/register', 'Auth\VendorRegisterController@showRegisterForm')->name('vendor.register');
+    Route::post('/register', 'Auth\VendorRegisterController@register')->name('vendor.register.submit');
+});
